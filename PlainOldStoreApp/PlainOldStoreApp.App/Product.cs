bfd3de659a501eddbd665d8579313aef9b7862dd
Melinda Waggoner <melinda.a.waggoner@gmail.com>
2021-12-10 09:05:12 -0600

    Add Product.cs

---
+++ PlainOldStoreApp/PlainOldStoreApp.App/Product.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PlainOldStoreApp.App
+{
+    internal class Product
+    {
+        internal int ProductId { get; set; }
+        internal string ProductName { get; set; }
+        internal decimal Price { get; set; }
+        internal string ProductDescription { get; set; }
+    }
+}
