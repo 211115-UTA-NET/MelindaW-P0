2f2ef643e9f11f13cfcc894c37ee39bf3e02540c
Melinda Waggoner <melinda.a.waggoner@gmail.com>
2021-12-10 09:03:50 -0600

    Add Customer.cs

---
+++ PlainOldStoreApp/PlainOldStoreApp.App/Customer.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PlainOldStoreApp.App
+{
+    internal class Customer
+    {
+        internal int CustomerId { get; set; }
+        internal string FirstName { get; set; }
+        internal string LastName { get; set; }
+        internal string Address { get; set; }
+        internal string City { get; set; }
+        internal string State { get; set; }
+        internal int ZipCode { get; set; }
+        internal string Email { get; set; }
+    }
+}
