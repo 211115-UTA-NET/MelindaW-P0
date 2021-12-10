9506beba6c2f35216aaa7437784338a0b3066a51
Melinda Waggoner <melinda.a.waggoner@gmail.com>
2021-12-10 09:05:58 -0600

    Add Store.cs

---
+++ PlainOldStoreApp/PlainOldStoreApp.App/Store.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PlainOldStoreApp.App
+{
+    internal class Store
+    {
+        internal int StoreId { get; set; }
+        internal string StoreName { get; set; }
+        internal string StoreAddress { get; set; }
+        internal string StoreCity { get; set; }
+        internal string StoreState { get; set; }
+        internal int StoreZipCode { get; set; }
+    }
+}
