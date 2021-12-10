dd96c3f554757541ae8271b59c541cb0c7660fd0
Melinda Waggoner <melinda.a.waggoner@gmail.com>
2021-12-10 09:04:31 -0600

    Add Order.cs

---
+++ PlainOldStoreApp/PlainOldStoreApp.App/Order.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PlainOldStoreApp.App
+{
+    internal class Order
+    {
+        internal int OrderId { get; set; }
+        internal DateTime DateTime { get; set; }
+    }
+}
