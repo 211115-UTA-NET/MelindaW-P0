9afb5329b563645a01101a55e7a76f0fb7e582ef
Melinda Waggoner <melinda.a.waggoner@gmail.com>
2021-12-15 10:20:10 -0600

    Add constructors to Customer class

--- PlainOldStoreApp/PlainOldStoreApp.App/Customer.cs
+++ PlainOldStoreApp/PlainOldStoreApp.App/Customer.cs
@@ -8,13 +8,28 @@ namespace PlainOldStoreApp.App
 {
     internal class Customer
     {
-        internal int CustomerId { get; set; }
+        internal Customer ()
+        {
+
+        }
+        internal Customer (string firstName, string lastName, string address, string city, string state, int zipCode, string email, Store store)
+        {
+            FirstName = firstName;
+            LastName = lastName;
+            Address = address;
+            City = city;
+            State = state;
+            ZipCode = zipCode;
+            Email = email;
+            Store = store;
+        }
         internal string FirstName { get; set; }
         internal string LastName { get; set; }
         internal string Address { get; set; }
         internal string City { get; set; }
         internal string State { get; set; }
         internal int ZipCode { get; set; }
-        internal string Email { get; set; }
+        internal string Email{ get; set; }
+        internal Store Store { get; set; }
     }
 }
