0ceeb0dd0e4e6b521899e63fead1d6a28ba875c9
Melinda Waggoner <melinda.a.waggoner@gmail.com>
2021-12-19 19:30:50 -0600

    Update to order.cs and program.cs

--- PlainOldStoreApp/PlainOldStoreApp.App/Order.cs
+++ PlainOldStoreApp/PlainOldStoreApp.App/Order.cs
@@ -8,7 +8,12 @@ namespace PlainOldStoreApp.App
 {
     internal class Order
     {
-        internal int OrderId { get; set; }
-        internal DateTime DateTime { get; set; }
+        private int OrderLineID = 0;
+        internal int OrderID { get; set; }
+        internal int StoreID { get; set; }
+        internal Guid CustomerID { get; set; }
+        internal DateTime OrderTime { get; set; }
+        internal int ProductID { get; set; }
+        internal int Quantity { get; set; }
     }
 }
--- PlainOldStoreApp/PlainOldStoreApp.App/Program.cs
+++ PlainOldStoreApp/PlainOldStoreApp.App/Program.cs
@@ -23,7 +23,7 @@ namespace PlainOldStoreApp
                 {
                     case "1":
                     case "place order":
-                        PlaceOrder();
+                        PlaceOrder.PlaceOrderMenu();
                         break;
                     case "2":
                     case "lookup order":
@@ -39,51 +39,6 @@ namespace PlainOldStoreApp
                 }
             }
 
-        }
-        private static void PlaceOrder()
-        {
-            bool isOrdering = true;
-            while (isOrdering)
-            {
-                Console.WriteLine("Would you like to place an order for a new or existing customer?");
-                Console.WriteLine("1. New Customer\n2. Existing Customer\n3. Done");
-                string? selection = Console.ReadLine()?.Trim().ToLower();
-                Console.WriteLine();
-
-                switch (selection)
-                {
-                    case "1":
-                    case "new customer":
-                        Console.WriteLine("Please enter in the customer's email.");
-                        string? email = Console.ReadLine()?.Trim();
-                        if(string.IsNullOrWhiteSpace(email))
-                        {
-                            break;
-                        }
-                        Tuple<bool, string> emailTuple = Validate.ValidateEmail(email);
-                        if (emailTuple.Item1 == false)
-                        {
-                            break;
-                        }
-                        Customer customer = new Customer();
-                        customer.Email = emailTuple.Item2;
-                        Console.WriteLine(customer.Email);
-                        //Check if email already exists
-                        //If email can not be found add new customer
-                        break;
-                    case "2":
-                    case "existing customer":
-                        break;
-                    case "3":
-                    case "done":
-                        Console.WriteLine("Thanks for placing an order.");
-                        isOrdering = false;
-                        break;
-                    default:
-                        Console.WriteLine("Invalid input. Please select a valid menu option.");
-                        break;
-                }
-            }
         }
     }
 }
